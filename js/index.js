7ab9ff4a9186b561c01508eaa299a77e75586f1e
realeve@qq.com <realeve@qq.com>
2018-06-13 21:40:50 +0800

    更新至demo版

--- js/index.js
+++ js/index.js
@@ -1,90 +1,91 @@
 var $ = require('./jquery');
+var CountUp = require('./countUp');
+var candle = require('./candle');
+require('./jquery.fullpage.js');
+
+
+var fired = false;
+
+function initEvent() {
+  $('#word').on('click', function () {
+    if (fired) {
+      return;
+    }
+    $('.section:nth(2)').removeClass('hide');
+
+    fired = true;
+    $('.candle').each(function (idx, $obj) {
+      setTimeout(function () {
+        $($obj).show();
+      }, 200 + idx * 300);
+    })
+  })
+
+  setTimeout(function () {
+    $('#word').removeClass('bounceInRight').addClass('pulse').css('animation-iteration-count', 'infinite');
+  }, 1000);
+}
+
+function initCandle() {
+  $('.candle').hide();
+  candle($("#surface1")[0]);
+  candle($("#surface2")[0]);
+  candle($("#surface3")[0]);
+}
+
+function initThree() {
+  setTimeout(function () {
+    $('.three').parent().removeClass('bounceInDown').addClass('pulse').css('animation-iteration-count', 'infinite');
+    $('.happy').removeClass('bounceInDown').addClass('rubberBand').css({
+      'animation-iteration-count': 'infinite',
+      'animation-duration': '2s'
+    });
+  }, 2000)
+}
+
+function initPage3() {
+  setTimeout(function () {
+    $('.beibei').removeClass('bounceInRight').addClass('pulse').css('animation-iteration-count', 'infinite');
+    startCounter(518);
+  }, 1500)
+}
+
+function startCounter(endValue) {
+  var options = {  
+    useEasing: true,
+      useGrouping: true,
+      separator: '',
+    //   separator: ',',
+      decimal: '.',
+  };
+  var counter = new CountUp('counter', 0, endValue, 0, 2.5, options);
+  counter.start();
+}
+
+function initFullpage() {
+  $('#fullpage').fullpage({
+    anchors: ['slide1', 'slide2', 'slide3'],
+    onLeave: function (index, curIndex, direction) {
+
+      var loadedSlide = $(this);
+      if (curIndex == 2) {
+        $('.page2').removeClass('hide');
+        initThree();
+        initEvent();
+      } else if (curIndex == 3) {
+        if (!fired) {
+          return;
+        }
+        $('.page3').removeClass('hide');
+        initPage3();
+      }
+    },
+    afterRender: function () {
+      initCandle();
+    },
+  });
+}
 
 $(document).ready(function () {
-  $('.candle').hide();
-  setTimeout(function () {
-    $('.candle').show();
-  }, 2600);
-  setTimeout(function () {
-    $('#slogan').addClass('rubberBand');
-  }, 1000);
-  // Set canvas drawing surface
-  var space = document.getElementById("surface");
-  var surface = space.getContext("2d");
-  surface.scale(1, 1);
-
-  // Set Particles
-  var particles = [];
-  var particle_count = 150;
-  for (var i = 0; i < particle_count; i++) {
-    particles.push(new particle());
-  }
-  var time = 0;
-  // shim layer with setTimeout fallback from Paul Irish
-  window.requestAnimFrame = (function () {
-    return window.requestAnimationFrame ||
-      window.webkitRequestAnimationFrame ||
-      window.mozRequestAnimationFrame ||
-      function (callback) {
-        window.setTimeout(callback, 6000 / 60);
-      };
-  })();
-
-  function particle() {
-
-    this.speed = {
-      x: -1 + Math.random() * 2,
-      y: -5 + Math.random() * 5
-    };
-    canvasWidth = (document.getElementById("surface").width);
-    canvasHeight = (document.getElementById("surface").height);
-    this.location = {
-      x: canvasWidth / 2,
-      y: (canvasHeight / 2) + 35
-    };
-
-    this.radius = .5 + Math.random() * 1;
-
-    this.life = 10 + Math.random() * 10;
-    this.death = this.life;
-
-    this.r = 255;
-    this.g = Math.round(Math.random() * 155);
-    this.b = 0;
-  }
-
-  function ParticleAnimation() {
-    surface.globalCompositeOperation = "copy";
-    surface.fillRect(0, 0, canvasWidth, canvasHeight);
-    surface.globalCompositeOperation = "lighter";
-
-    for (var i = 0; i < particles.length; i++) {
-      var p = particles[i];
-
-      surface.beginPath();
-
-      p.opacity = Math.round(p.death / p.life * 100) / 100
-      var gradient = surface.createRadialGradient(p.location.x, p.location.y, 0, p.location.x, p.location.y, p.radius);
-      gradient.addColorStop(0, "rgba(" + p.r + ", " + p.g + ", " + p.b + ", " + p.opacity + ")");
-      gradient.addColorStop(0.5, "rgba(" + p.r + ", " + p.g + ", " + p.b + ", " + p.opacity + ")");
-      gradient.addColorStop(1, "rgba(" + p.r + ", " + p.g + ", " + p.b + ", 0)");
-      surface.fillStyle = gradient;
-      surface.arc(p.location.x, p.location.y, p.radius, Math.PI * 2, false);
-      surface.fill();
-      p.death--;
-      p.radius++;
-      p.location.x += (p.speed.x);
-      p.location.y += (p.speed.y);
-
-      //regenerate particles
-      if (p.death < 0 || p.radius < 0) {
-        //a brand new particle replacing the dead one
-        particles[i] = new particle();
-      }
-    }
-    requestAnimFrame(ParticleAnimation);
-  }
-
-  ParticleAnimation();
-
+  initFullpage();
 });
